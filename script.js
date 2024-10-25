f64625fb77ce3f2c490ffd0ceca0ee1afebf8d1c
FejiroJoan <fejiroadios69@gmail.com>
2024-10-25 08:22:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// function addTask(){
+//     if(input)
+// }
+
+
+
+
+// const inputBox = document.getElementById('input-box');
+// const listContainer = document.getElementById('list-container');
+
+// function addTask(){
+//     if(inputBox === 'r'){
+//         alert('you must write something');
+//     }
+//     else{
+//         let li = document.createElement('li');
+//         li.innerHTML = inputBox.value;
+//         listContainer.appendChild(li);
+//     }
+// }
